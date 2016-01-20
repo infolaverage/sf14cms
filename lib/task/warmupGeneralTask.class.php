7dc5cce62de7e8ef38a92af812bfb5ec19429ee0
szilka88 <szilka88@gmail.com>
2016-01-20 19:37:48 +0100

    collecting basic option keys vol 2...

--- lib/task/warmupGeneralTask.class.php
+++ lib/task/warmupGeneralTask.class.php
@@ -33,16 +33,79 @@ EOF;
     $databaseManager = new sfDatabaseManager($this->configuration);
     $connection = $databaseManager->getDatabase($options['connection'])->getConnection();
 
+    // MOBILE:
+    //====================================
+    //ionic_application_id
+    //ionic_application_public_key
+    //ionic_application_secret_key
+    //api_version_current
+    //api_version_available
+
+    //view_cache_enabled
+    //view_cache_delay
+
+    //social_facebook_page
+    //social_twitter_page
+    //social_pinterest_page
+    //social_youtube_page
+    //social_googleplus_page
+
+    //robots_txt
+    //sitemap_enabled
+
+    //seo_meta_title
+    //seo_html_title
+    //seo_meta_description
+    //seo_meta_robots
+    //seo_geo_region
+    //seo_geo_placename
+    //seo_geo_position_lat
+    //seo_geo_position_lng
+    //seo_geo_title
+
+    //google_analytics_ua_code
+    //google_analytics_enabled
+    //social_addthis_enabled
+    //social_addthis_app_id
 
 
-      //option_keys
+    // I18N
+    //====================================
+    //i18n_available_cultures
+    //i18n_available_cultures_default
 
-      //ionic_application_id
-      //ionic_application_public_key
-      //ionic_application_secret_key
+    // INDEX SLUGS
+    //====================================
+    //index_slug_contact
+    //index_slug_gallery
+    //index_slug_faq
+    //index_slug_blog_entry
+    //index_slug_team_member
+    //index_slug_service
+    //index_slug_reference
+    //index_slug_product
 
-      //view_cache_enabled
-      //view_cache_delay
+    // MAIL SLUGS
+    //====================================
+    //mail_noreply_address
+
+    /*
+    site_menu_top_id
+    contact_email_address_1
+    contact_phone_1
+    social_facebook_app_id
+    contact_address_1
+    organization_name
+    organization_main_office
+    organization_company_registration_number
+    contact_map_html_1
+    site_brand_name
+    contact_has_attachment
+    max_per_page_blog_entry
+    mail_contact_address
+    mail_contact_subject
+    site_menu_footer_id
+    */
 
   }
 }
