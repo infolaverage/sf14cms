f630c8be569da84ba94da251bde919bb6afa3cb7
szilka88 <szilka88@gmail.com>
2016-01-20 18:35:42 +0100

    test email valdiator file

---
+++ lib/validator/myValidatorAdditionalEmail.class.php
@@ -0,0 +1,14 @@
+<?php
+class myValidatorAdditionalEmail extends sfValidatorRegex
+{
+    const REGEX_EMAIL = '/^([^áéíóöőúüűÁÉÍÓÖŐÚÖŰ()<>[\\]%\,:;@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i';
+    /**
+     * @see sfValidatorRegex
+     */
+    protected function configure($options = array(), $messages = array())
+    {
+        parent::configure($options, $messages);
+
+        $this->setOption('pattern', self::REGEX_EMAIL);
+    }
+}
