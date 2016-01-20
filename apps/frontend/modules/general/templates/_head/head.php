0ab2218f15021dbceb2d85524cd84c0fc801331f
szilka88 <szilka88@gmail.com>
2016-01-20 18:52:08 +0100

    sfcombine plugin installation

--- apps/frontend/modules/general/templates/_head/head.php
+++ apps/frontend/modules/general/templates/_head/head.php
@@ -1,17 +1,20 @@
-<?php if(0/* && Project::getGlobalEnvironmentIs("prod")*/):?>
-    <?php //include_combined_stylesheets() ?>
-    <?php //include_combined_javascripts() ?>
-<?php else:?>
+<?php
+/** @var sfGuardUser $sf_user */
+if($sf_user->isAuthenticated() && $sf_user->getGuardUser() && $sf_user->getGuardUser()->getIsSuperAdmin()) {
+    use_javascript("/resources_core/plugins/jquery-ui/jquery-ui.min.js");
+}
+?>
 
-    <?php
-    /**
-     * @var sfGuardUser $sf_user
-    */?>
-    <?php if($sf_user->isAuthenticated() && $sf_user->getGuardUser()->getIsSuperAdmin()):?>
-        <?php use_javascript("/resources_core/plugins/jquery-ui/jquery-ui.min.js");?>
-    <?php endif;?>
+<?php if(
+    (sfConfig::get("sf_environment") != "dev") ||  # if production
+    Project::getGlobalEnvironmentIs("prod")        # or domain is production
+):?>
+    <?php include_combined_stylesheets() ?>
+    <?php include_combined_javascripts() ?>
+<?php else:?>
     <?php include_stylesheets()?>
     <?php include_javascripts()?>
 <?php endif;?>
 
-<?php include_partial("general/other/google_analytics/uacode")?>
+<?php include_partial("general/other/google_analytics/uacode");?>
+<?php #include_partial("general/other/hotjar/tracking_all")?>
---
+++ web/backend_dev_stdxb.php
@@ -0,0 +1,38 @@
+<?php
+$redirect_to_prod = true;
+
+if (!in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1')))
+{
+   #die('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+}
+
+if(isset($_SERVER['REMOTE_ADDR']) && isset($_SERVER['SERVER_ADDR'])){
+    if(
+        (strpos($_SERVER['REMOTE_ADDR'], "10.1.2.2") === 0) ||
+        (strpos($_SERVER['SERVER_ADDR'], "10.1.2.8") === 0)
+    ) {
+        $redirect_to_prod = false;
+    }
+}
+
+if($redirect_to_prod){
+
+    $dev_filename = "/".basename(__FILE__);
+    #echo "red:".$redirect_to_prod;
+    #echo "red:".$dev_filename ;
+    #echo $_SERVER["REQUEST_URI"];
+    #exit;
+
+    if(strpos($_SERVER["REQUEST_URI"], $dev_filename) !== false){
+        $new_uri = str_replace($dev_filename, "", $_SERVER["REQUEST_URI"]);
+        $new = "http://".$_SERVER["SERVER_NAME"].$new_uri ;
+        header('HTTP/1.1 301 Moved Permanently');
+        header('Location: ' . $new);
+        exit;
+    }
+}
+
+require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
+
+$configuration = ProjectConfiguration::getApplicationConfiguration('backend', 'dev', true);
+sfContext::createInstance($configuration)->dispatch();
